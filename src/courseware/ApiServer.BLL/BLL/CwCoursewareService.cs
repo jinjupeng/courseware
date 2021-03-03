e5ee4a65f5e6ed1b38af86bdd464fe235474df14
jinjupeng <im.jp@outlook.com>
2021-03-03 22:05:01 +0800

    Service层实现

---
+++ src/courseware/ApiServer.BLL/BLL/CwCoursewareService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiServer.BLL.BLL
+{
+    public class CwCoursewareService : ICwCoursewareService
+    {
+    }
+}
---
+++ src/courseware/ApiServer.BLL/BLL/SysRolePermissionService.cs
@@ -0,0 +1,11 @@
+﻿using ApiServer.BLL.IBLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiServer.BLL.BLL
+{
+    class SysRolePermissionService : ISysRolePermissionService
+    {
+    }
+}
